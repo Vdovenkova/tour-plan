0a80bb4f8d78d071aedc9d8e3a52f3aed9dd6150
Vdovenkova <ivdovenkova@mail.ru>
2020-08-08 16:00:30 +0300

    create: added api ya.maps

--- js/main.js
+++ js/main.js
@@ -19,3 +19,35 @@ var mySwiper = new Swiper(".swiper-container", {
     pageUpDown: false,
   },
 });
+
+// API яндекс.карт
+ymaps.ready(init);
+function init() {
+  // Создание карты.
+  var myMap = new ymaps.Map("map", {
+      // Координаты центра карты.Порядок по умолчанию: «широта, долгота».
+      center: [12.93, 100.88],
+      // Уровень масштабирования. Допустимые значения:от 0 (весь мир) до 19.
+      zoom: 13,
+    }),
+    // Создаем геообъект с типом геометрии "Точка".
+    myGeoObject = new ymaps.GeoObject(
+      {
+        // Описание геометрии.
+        geometry: {
+          type: "Point",
+          coordinates: [12.93, 100.88],
+        },
+        // Свойства.
+        properties: {
+          // Контент метки.
+          iconContent: "Hilton Pattaya",
+        },
+      },
+      {
+        // Опции. Иконка метки будет растягиваться под размер ее содержимого.
+        preset: "islands#redStretchyIcon",
+      }
+    );
+  myMap.geoObjects.add(myGeoObject);
+}
