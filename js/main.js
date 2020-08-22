85722362451cd5af6b1a5d2ff036248fa8be4901
Vdovenkova <ivdovenkova@mail.ru>
2020-08-22 19:55:50 +0300

    create: added mobile menu

--- js/main.js
+++ js/main.js
@@ -73,3 +73,10 @@ var reviewsSlider = new Swiper(".reviews-slider", {
     pageUpDown: false,
   },
 });
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  document
+    .querySelector(".navbar-bottom")
+    .classList.toggle("navbar-bottom--visible");
+});
